8b209b1109e4d8641c30fa62c34acc514eae0405
Rafe Dunlop <rdu46@uclive.ac.nz>
2024-08-12 11:00:54 +1200

    resolved warnings and enhanced documentation

--- src/main/java/seng202/team3/gui/Dummy2Controller.java
+++ src/main/java/seng202/team3/gui/Dummy2Controller.java
@@ -3,11 +3,22 @@ package seng202.team3.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+/**
+ * controller for dummy1.fxml, not for release
+ * @author Rafe Dunlop (rdu46)
+ */
 public class Dummy2Controller {
 
+    /**
+     * TextField to write to, demonstrating that the controller
+     * can operate without wresting control from navbar
+     */
     @FXML
     private TextField text;
 
+    /**
+     * initializes the controller, setting a default value to the TextField
+     */
     public void initialize() {
         text.setText("Dummy2!");
     }
