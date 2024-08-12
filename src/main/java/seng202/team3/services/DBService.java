3f26f4b153511013acc9552f50df8aefcb0b37ad
Steven Leishman <sle159@uclive.ac.nz>
2024-08-12 12:59:42 +1200

    Added javadoc to DBService class - no tests as no functionality yet

--- src/main/java/seng202/team3/services/DBService.java
+++ src/main/java/seng202/team3/services/DBService.java
@@ -1,8 +1,26 @@
 package seng202.team3.services;
 import seng202.team3.models.*;
 import java.util.*;
+
+/**
+ *This class represents the service which requests data from the database
+ * and supplies it back to the models
+ *
+ * @author Steven Leishman
+ */
 public class DBService {
 
+
+    /**
+     * searches DB for users record under username
+     * returns list of strings of users saved data
+     * or if no data exists throw exception
+     *
+     * @param username - users username to search
+     * @param password
+     * @return List of Strings of user data
+     * @throws WineDrinkerDoesNotExistException
+     */
     public List<String>  readDrinkerDetails(String username, String password) throws WineDrinkerDoesNotExistException{
         boolean mockFlag = false;
         if (mockFlag) {
@@ -12,13 +30,31 @@ public class DBService {
         return new ArrayList<>();
     }
 
+    /**
+     * writes a wineDrinker record to the database from input
+     * wineDrinker instance
+     * @param wineDrinker
+     */
     public void writeDrinkerDetails(WineDrinker wineDrinker){
-        //to implement
+        //to implement when SQL learned
     }
+
+    /**
+     * Creates a new wineDrinker record with input username and password
+     * utilises writeDrinkerDetails
+     * @param username
+     * @param password
+     */
     public void writeNewDrinkerRecord(String username, String password){
         System.out.println("Wine Drinker record created");
+        //writeDrinkerDetails();
     }
+
+    /**
+     * Adds inputted wineList to db
+     * @param wineList
+     */
     public void writeWineList(List<Wine> wineList){
-        //to implement
+        //to implement when SQL learned
     }
 }
