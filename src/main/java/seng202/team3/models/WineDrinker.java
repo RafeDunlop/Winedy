0d3f849b6174f1da108549e47af5885544d81a32
Steven Leishman <sle159@uclive.ac.nz>
2024-08-12 13:41:50 +1200

    Javadoc added to WineDrinker Class

--- src/main/java/seng202/team3/models/WineDrinker.java
+++ src/main/java/seng202/team3/models/WineDrinker.java
@@ -1,7 +1,7 @@
 package seng202.team3.models;
 
 import seng202.team3.services.*;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * WineDrinker class holds important information about the user
@@ -20,6 +20,10 @@ public class WineDrinker {
     boolean isLoggedIn = false;
     DBService dbService;
 
+
+    /**
+     * Initialise Blank WineDrinker object
+     */
     public WineDrinker() {
         this.username = null;
         this.wineColourPreference = null;
@@ -28,38 +32,86 @@ public class WineDrinker {
 
     }
 
+    /**
+     * Initialises WineDrinker object and DBService instance and stores
+     * data retrieved from database under given username and password
+     * @param username username for database search record
+     * @param password password to authenticate user
+     */
     public WineDrinker(String username, String password) {
         this.username = username;
         this.dbService = new DBService();
+        List<String> retrievedWineDrinkerData = new ArrayList<>();
         try {
-            dbService.readDrinkerDetails(username, password);
+            retrievedWineDrinkerData = dbService.readDrinkerDetails(username, password);
         } catch (WineDrinkerDoesNotExistException e){
             e.printStackTrace();
             dbService.writeNewDrinkerRecord(username, password);
         }
+        for (int i = 0; i < retrievedWineDrinkerData.size(); i++){
+            //do stuff with the data
+        }
     }
 
+    /**
+     * Reads preferences of user
+     * TODO DISCUSS VALIDITY OF THIS, POSSIBLE REDUNDANCY
+     */
     void readPreferences() {
-
+        //make a request to db for preferences
     }
 
+
+    /**
+     * Creates new userWineList and adds it to drinkersWineList
+     * TODO add input from on screen text box as name
+     */
     void createWineList() {
+        WineList wineList = new UserWineList();
+        drinkersWineLists.add(wineList);
     }
 
-    void removeWineList(WineList list) {
+    /**
+     * Removes winelist from users stored data
+     * TODO add service call to delete record from database
+     * @param wineListToRemove
+     */
+    void removeWineList(WineList wineListToRemove) {
         for (WineList wineList : drinkersWineLists) {
             //need wineList.getName()
             //drinkersWineLists.remove(wineList)
         }
     }
 
+    /**
+     * Returns the username of current instance of WineDrinker
+     * @return this.username
+     */
     public String getUsername(){
         return this.username;
     }
+
+    /**
+     * Returns the colour preference of current instance of WineDrinker
+     * @return this.wineColourPreference
+     */
     public String getWineColourPreference(){
         return this.wineColourPreference;
     }
+
+    /**
+     * Returns the wineFullness of current instance of WineDrinker
+     * @return this.wineFullnessPreference
+     */
     public String getWineFullnessPreference(){
         return this.wineFullnessPreference;
+    }
+
+    /**
+     * sets username to inputted value
+     * @param username username to set to
+     */
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
