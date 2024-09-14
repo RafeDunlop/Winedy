f20d0f934779a8cf34ca7ca878e6559e9e4d60dc
Yfa50 <yfa50@uclive.ac.nz>
2024-09-14 20:13:53 +1200

    Created ProfileScreenService class to be used by the ProfileScreenController. Implemented constructor and savePreferences method which saves the selected preferences to the database using WineDrinkerDAO.

---
+++ src/main/java/seng202/team3/guiservice/ProfileScreenService.java
@@ -0,0 +1,29 @@
+package seng202.team3.guiservice;
+
+import seng202.team3.WineDrinkerManager;
+import seng202.team3.models.WineDrinker;
+import seng202.team3.services.WineDrinkerDAO;
+
+/**
+ * Service class for the profile screen
+ * @author Yuvraj (yfa50)
+ */
+public class ProfileScreenService {
+
+    private WineDrinkerManager wineDrinkerManager;
+    private WineDrinkerDAO wineDrinkerDAO;
+
+    public ProfileScreenService() {
+        this.wineDrinkerManager = WineDrinkerManager.getInstance();
+        this.wineDrinkerDAO = new WineDrinkerDAO();
+    }
+
+    public void savePreferences(String colour, String fullness, String variety, double abvLimit){
+        WineDrinker wineDrinker = wineDrinkerManager.getCurrentUser();
+        wineDrinker.setColourPreference(colour);
+        wineDrinker.setFullnessPreference(fullness);
+        wineDrinker.setVarietyPreference(variety);
+        wineDrinker.setAbvLimit(abvLimit);
+        wineDrinkerDAO.update(wineDrinker);
+    }
+}
