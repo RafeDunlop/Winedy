7c09960368cc77d159ae006074d449d0a6be96f1
nsr36 <nsr36@uclive.ac.nz>
2024-09-15 18:04:43 +1200

    Begun implementing WineDAOTest, tested add function in WineDAO Also fixed JavaFX version issue by editing build.gradle

--- src/test/java/seng202/team3/unittests/services/WineDAOTest.java
+++ src/test/java/seng202/team3/unittests/services/WineDAOTest.java
@@ -1,4 +1,59 @@
 package seng202.team3.unittests.services;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seng202.team3.models.Wine;
+import seng202.team3.services.DatabaseManager;
+import seng202.team3.services.WineDAO;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WineDAOTest {
+    private WineDAO wineDAO = new WineDAO();
+    private DatabaseManager databaseManager;
+
+    @BeforeEach
+    public void setup() {
+        String databasePath = "./build/classes/java/database.db";
+        File databaseFile = new File(databasePath);
+        if (databaseFile.exists()) {
+            if (databaseFile.delete()) {
+                System.out.println("Database file deleted successfully.");
+            } else {
+                System.out.println("Failed to delete the database file.");
+            }
+        }
+        DatabaseManager.REMOVE_INSTANCE();
+        databaseManager = DatabaseManager.getInstance();
+        databaseManager.resetDB();
+    }
+
+    @AfterEach
+    public void removeInstance() {
+        DatabaseManager.REMOVE_INSTANCE();
+    }
+
+    @Test
+    void testAdd() {
+        Wine wine = new Wine(
+                783,
+                "Nero Oro Appassimento 2018, Sicily",
+                "Italy",
+                "Red",
+                "Big",
+                new String[]{"Nero d'Avola"},
+                "FULL",
+                "Nero Oro is made by a winemaker who's scored a perfect 100 Parker Points.",
+                (float) 9.99,
+                new String[]{"IWC 2019 - Commended Award", "Decanter 2019 - Bronze Award"},
+                (float) 14,
+                75,
+                2018);
+        int insertId = wineDAO.add(wine);
+        assertEquals(insertId, 783);
+    }
+
 }
