416798c9e68d9fb88c2a2ceca6ad87d1cf3033ce
Yfa50 <yfa50@uclive.ac.nz>
2024-09-19 20:43:19 +1200

    Updated initialise_wine_database.sql to match Winedy_ER_Model_V2.

--- src/main/resources/sql/initialise_wine_database.sql
+++ src/main/resources/sql/initialise_wine_database.sql
@@ -1,4 +1,14 @@
-CREATE TABLE wine (
+CREATE TABLE wineDrinker (
+    username TEXT PRIMARY KEY,
+    password TEXT,
+    countryPreference TEXT,
+    colourPreference TEXT,
+    fullnessPreference TEXT,
+    grapePreference TEXT,
+    abvLimit DOUBLE
+);
+--Split
+CREATE TABLE wineSuper (
     id INTEGER PRIMARY KEY,
     name TEXT,
     country TEXT,
@@ -9,42 +19,70 @@ CREATE TABLE wine (
     pricePerBottle FLOAT,
     alcoholByVolume FLOAT,
     volumeInML FLOAT,
-    year INT);
+    year INT
+);
 --Split
-CREATE TABLE wineDrinker (
-    username TEXT PRIMARY KEY,
-    password TEXT,
-    countryPreference TEXT,
-    colourPreference TEXT,
-    fullnessPreference TEXT,
-    grapePreference TEXT,
-    abvLimit double);
+CREATE TABLE wine (
+    id INTEGER PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES wineSuper (id)
+);
+--Split
+CREATE TABLE personalWine (
+    id INTEGER PRIMARY KEY,
+    wineDrinker TEXT,
+    FOREIGN KEY (id) REFERENCES wineSuper (id),
+    FOREIGN KEY (wineDrinker) REFERENCES wineDrinker (username)
+);
 --Split
 CREATE TABLE wineList (
     name TEXT,
-    userId INTEGER,
-    PRIMARY KEY(name, userId),
-    FOREIGN KEY(userId) REFERENCES wineDrinker(username) );
+    username TEXT,
+    description TEXT,
+    PRIMARY KEY (name, username),
+    FOREIGN KEY (username) REFERENCES wineDrinker (username)
+);
 --Split
 CREATE TABLE contains (
     note TEXT,
     wineId INTEGER,
     listName TEXT,
-    wineDrinkerId INTEGER,
-    PRIMARY KEY(wineId, listName, wineDrinkerId),
-    FOREIGN KEY(wineId) REFERENCES wine(id),
-    FOREIGN KEY(listName, wineDrinkerId) REFERENCES wineList(name, userId) );
+    wineDrinker TEXT,
+    PRIMARY KEY (wineId, listName, wineDrinker),
+    FOREIGN KEY (wineId) REFERENCES wine (id),
+    FOREIGN KEY (listName, wineDrinker) REFERENCES wineList (name, username)
+);
+--Split
+CREATE TABLE writesNoteAbout (
+    wineDrinker TEXT,
+    wineId INTEGER,
+    note TEXT,
+    PRIMARY KEY (wineDrinker, wineId),
+    FOREIGN KEY (wineDrinker) REFERENCES wineDrinker (username),
+    FOREIGN KEY (wineId) REFERENCES wineSuper (id)
+);
+--Split
+CREATE TABLE logs (
+    wineDrinker TEXT,
+    wineId INTEGER,
+    logEntry TEXT,
+    date DATE,
+    time TIME,
+    quantity FLOAT,
+    PRIMARY KEY (wineDrinker, wineId),
+    FOREIGN KEY (wineDrinker) REFERENCES wineDrinker (username),
+    FOREIGN KEY (wineId) REFERENCES wineSuper (id)
+);
 --Split
 CREATE TABLE grape (
     wineId INTEGER,
     name TEXT,
     PRIMARY KEY (wineId, name),
-    FOREIGN KEY (wineId) REFERENCES wine
+    FOREIGN KEY (wineId) REFERENCES wineSuper (id)
 );
 --Split
 CREATE TABLE award (
     wineId INTEGER,
     name TEXT,
     PRIMARY KEY (wineId, name),
-    Foreign KEY (wineId) REFERENCES wine
+    Foreign KEY (wineId) REFERENCES wineSuper (id)
 );
