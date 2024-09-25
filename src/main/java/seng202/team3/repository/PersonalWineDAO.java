6e711376d629a74ee966cbde3cf6d7af19dc7c76
nsr36 <nsr36@uclive.ac.nz>
2024-09-25 19:33:05 +1200

    Implemented getAll method in PersonalWineDAO

--- src/main/java/seng202/team3/repository/PersonalWineDAO.java
+++ src/main/java/seng202/team3/repository/PersonalWineDAO.java
@@ -1,7 +1,10 @@
 package seng202.team3.repository;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import seng202.team3.exceptions.WineDrinkerAlreadyExistsException;
 import seng202.team3.models.Wine;
+import seng202.team3.services.WineDrinkerManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * PersonalWine DAO Class that handles all personal wine related actions to the database
@@ -18,14 +22,25 @@ import java.util.List;
 public class PersonalWineDAO implements DAOInterface<Wine> {
 
     /**
+     * Logger for robust error logging
+     */
+    private static final Logger log = LogManager.getLogger(WineDAO.class);
+
+    /**
      * Variable to store DatabaseManager instance to manage database connections
      */
     private final DatabaseManager databaseManager;
 
     /**
+     * Variable to store WineDAO instance to ______________
+     */
+    private final WineDAO wineDAO;
+
+    /**
      * Creates a new PersonalWineDAO object and gets a reference to the database singleton
      */
     public PersonalWineDAO() {
+        wineDAO = new WineDAO();
         databaseManager = DatabaseManager.getInstance();
     }
 
@@ -33,6 +48,7 @@ public class PersonalWineDAO implements DAOInterface<Wine> {
      * PersonalWineDAO constructor with ability to pass in a database url
      */
     public PersonalWineDAO(String url) {
+        wineDAO = new WineDAO(url);
         databaseManager = DatabaseManager.getInstance(url);
     }
 
@@ -43,7 +59,24 @@ public class PersonalWineDAO implements DAOInterface<Wine> {
      */
     @Override
     public List<Wine> getAll() {
-        return null;
+        List<Wine> wines = new ArrayList<>();
+        String sqlWine = "SELECT * FROM personalWine WHERE wineDrinker=?";
+        String currentWineDrinker = WineDrinkerManager.getInstance().getCurrentUser().getUsername();
+        try (Connection conn = databaseManager.connect();
+             PreparedStatement ps = conn.prepareStatement(sqlWine)) {
+            ps.setString(1, currentWineDrinker);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    Wine newWine = wineDAO.getWineByID(id);
+                    wines.add(newWine);
+                }
+                return wines;
+            }
+        } catch (SQLException sqlException) {
+            log.error(sqlException);
+            return new ArrayList<>();
+        }
     }
 
     /**
