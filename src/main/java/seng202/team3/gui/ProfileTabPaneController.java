0c65c0a84aab95f83e8681a52837425bbf99bcf4
sco207 <sco207@uclive.ac.nz>
2024-09-27 12:31:06 +1200

    Updated what the tab pane would look like based on some peer feedback (Alex Wilson, Isaac Macdonald) so it would make more sense to the user.

--- src/main/java/seng202/team3/gui/ProfileTabPaneController.java
+++ src/main/java/seng202/team3/gui/ProfileTabPaneController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
+import seng202.team3.services.WineDrinkerManager;
 
 public class ProfileTabPaneController {
     @FXML
@@ -24,7 +25,11 @@ public class ProfileTabPaneController {
     @FXML
     private AnchorPane consumptionTrackingAnchorPane;
 
+    @FXML
+    private Label helloUserLabel;
+
     public void initialize() {
+        helloUserLabel.setText("Welcome, " + WineDrinkerManager.getInstance().getCurrentUser().getUsername() + "!");
         FXWrapper.getInstance().loadProfileActionScreen(winedyProfileAnchorPane, Screen.PROFILESCREEN);
         FXWrapper.getInstance().loadProfileActionScreen(wineListAnchorPane, Screen.WINELISTSSCREEN);
         FXWrapper.getInstance().loadProfileActionScreen(consumptionTrackingAnchorPane, Screen.TRACKINGCONSUMPTIONSCREEN);
