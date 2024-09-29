95b8ac42f6258d06cad9d6ec8410820417743acf
sco207 <sco207@uclive.ac.nz>
2024-09-29 16:30:45 +1300

    Favourites list for a user is now created when a user logs in or registers

--- src/main/java/seng202/team3/services/WineDrinkerManager.java
+++ src/main/java/seng202/team3/services/WineDrinkerManager.java
@@ -100,6 +100,7 @@ public class WineDrinkerManager {
         try {
             if (currentUser != null) {
                 wineDrinkerDAO.add(currentUser);
+                WineListManager.getInstance().setupFavourites();
             }
         } catch (WineDrinkerAlreadyExistsException e) {
             log.error(e);
@@ -119,6 +120,7 @@ public class WineDrinkerManager {
             String[] saltPass = wineDrinker.getPassword().split(":");
             if (Password.check(password, saltPass[1]).addSalt(saltPass[0]).withBcrypt()) {
                 setCurrentUser(wineDrinker);
+                WineListManager.getInstance().setupFavourites();
             } else {
                 throw new IllegalWineDrinkerException("Password Incorrect");
             }
