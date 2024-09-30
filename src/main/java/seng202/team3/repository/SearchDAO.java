56ec4c2b72c2d9d1c0f220ec8d5ee0284473ad22
Yfa50 <yfa50@uclive.ac.nz>
2024-10-01 03:15:26 +1300

    Added SearchDAO functionality to validate column names and get all distinct values in a column.

--- src/main/java/seng202/team3/repository/SearchDAO.java
+++ src/main/java/seng202/team3/repository/SearchDAO.java
@@ -1,4 +1,92 @@
 package seng202.team3.repository;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Search DAO class that handles all wine related actions to the database
+ *
+ * @author Yuvraj Fagotra (yfa50)
+ */
 public class SearchDAO {
+
+    private String tableName = "wineSuper";
+
+    /**
+     * Logger for robust error logging
+     */
+    private static final Logger log = LogManager.getLogger(WineDAO.class);
+
+    /**
+     * Database manager instance to manage database connections
+     */
+    private final DatabaseManager databaseManager;
+
+    /**
+     * Creates a new WineDAO object and gets a reference to the database singleton
+     */
+    public SearchDAO() {
+        databaseManager = DatabaseManager.getInstance();
+    }
+
+    /**
+     * Creates a new WineDAO object and gets a reference to the database singleton for a database at the specified url.
+     * Used for testing.
+     */
+    public SearchDAO(String url) {
+        databaseManager = DatabaseManager.getInstance(url);
+    }
+
+
+    /**
+     * Gets all distinct values that occur in a given attribute (column) in the database
+     * @param attribute
+     * @return
+     */
+    public ArrayList<String> getWineAttributeValues(String attribute) {
+
+        ArrayList<String> values = new ArrayList<>();
+
+        String sql = "SELECT DISTINCT " + attribute + " FROM " + tableName;
+        try (Connection conn = databaseManager.connect();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ResultSet resultset = ps.executeQuery();
+
+            while (resultset.next()) {
+                String result = resultset.getString(attribute);
+                if (result != null && !result.isEmpty()) {
+                    values.add(resultset.getString(attribute));
+                }
+            }
+
+        } catch (SQLException sqlException) {
+            log.error("Error verifying attribute in database", sqlException);
+        }
+
+        return values;
+    }
+
+    public boolean isValidAttribute(String attribute) {
+
+        String sql = "SELECT name FROM pragma_table_info(?) WHERE name = ?";
+        try (Connection conn = databaseManager.connect();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, tableName);
+            ps.setString(2, attribute);
+            ResultSet resultSet = ps.executeQuery();
+            return resultSet.next();
+
+        } catch (SQLException sqlException) {
+            log.error("Error verifying attribute in database", sqlException);
+        }
+
+        return false;
+    }
+
 }
