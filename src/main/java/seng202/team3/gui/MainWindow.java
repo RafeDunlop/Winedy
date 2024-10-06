63163875408b5b1d422012662aa461214249cd15
Sophia Copley <sco207@uclive.ac.nz>
2024-10-06 17:56:42 +1300

    Styled the pop-ups so that they have the white wine coloured pane as their background for UI consistency. Finishing these pop-ups will require bolding the headings and styling the buttons etc

--- src/main/java/seng202/team3/gui/MainWindow.java
+++ src/main/java/seng202/team3/gui/MainWindow.java
@@ -54,7 +54,8 @@ public class MainWindow extends Application {
                 "/css/profile_tab_pane.css",
                 "/css/add_to_list_button.css",
                 "/css/like_button.css",
-                "/css/overlay.css"
+                "/css/overlay.css",
+                "/css/white_wine_pane.css"
         };
         try {
             for (String cssFilePath : cssPaths) {
