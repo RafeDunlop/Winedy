4bad1628a9db6c041ed159323fc7b1d3c87389cf
Yfa50 <yfa50@uclive.ac.nz>
2024-10-08 01:51:39 +1300

    Refactored the searching for wines so that it is run on a separate thread.

--- src/main/java/seng202/team3/gui/SearchScreenController.java
+++ src/main/java/seng202/team3/gui/SearchScreenController.java
@@ -1,7 +1,9 @@
 package seng202.team3.gui;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -163,32 +165,49 @@ public class SearchScreenController {
      */
     @FXML
     void onSearchButtonClicked(ActionEvent event) {
+        searchButton.setDisable(true);
         searchResultsVBox.getChildren().clear();
         FXWrapper.getInstance().clearPane(wineDetailsAnchorPane);
-        WineManager wineManager = WineManager.getInstance();
+        getSearchResults();
+    }
 
-        SearchWineList results = wineManager.searchWines(
-                searchBarTextField.getText(),
-                lowYear,
-                highYear,
-                (float) priceRangeSlider.getLowValue(),
-                (float) priceRangeSlider.getHighValue(),
-                !"All".equals(selectedCountry) ? selectedCountry : null,
-                !"All".equals(selectedColour) ? selectedColour : null,
-                !"All".equals(selectedFullness) ? selectedFullness : null,
-                !"All".equals(selectedVariety) ? selectedVariety : null);
-        List<Wine> resultsList = results.getWineList();
 
-        GuiService.startButtonGeneration(resultsList, searchResultsVBox, wineDetailsAnchorPane, 3);
+    private void getSearchResults() {
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                SearchWineList results = WineManager.getInstance().searchWines(
+                        searchBarTextField.getText(),
+                        lowYear,
+                        highYear,
+                        (float) priceRangeSlider.getLowValue(),
+                        (float) priceRangeSlider.getHighValue(),
+                        !"All".equals(selectedCountry) ? selectedCountry : null,
+                        !"All".equals(selectedColour) ? selectedColour : null,
+                        !"All".equals(selectedFullness) ? selectedFullness : null,
+                        !"All".equals(selectedVariety) ? selectedVariety : null);
 
-        if (results.getWineList().isEmpty()) {
-            infoTextLabel.setText("Unfortunately there were no results for your search. Try checking your spelling or broadening your filters.");
-            infoTextRectangle.setOpacity(1);
-            infoTextLabel.setOpacity(1);
-        } else {
-            infoTextRectangle.setOpacity(0);
-            infoTextLabel.setOpacity(0);
-        }
+                Platform.runLater(() -> {
+                    if (results.getWineList().isEmpty()) {
+                        infoTextLabel.setText("Unfortunately there were no results for your search. Try checking your spelling or broadening your filters.");
+                        infoTextRectangle.setOpacity(1);
+                        infoTextLabel.setOpacity(1);
+                    } else {
+                        infoTextRectangle.setOpacity(0);
+                        infoTextLabel.setOpacity(0);
+                    }
+                });
+
+                GuiService.startButtonGeneration(results.getWineList(), searchResultsVBox, wineDetailsAnchorPane, 3);
+                return null;
+            }
+        };
+
+        task.setOnSucceeded(e -> searchButton.setDisable(false));
+
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     /**
