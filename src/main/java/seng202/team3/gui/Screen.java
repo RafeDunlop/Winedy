7b2d8a7607c47747fcc171402e70f119c41c53d8
Hannah Botting (hbo51) <hbo51@uclive.ac.nz>
2024-10-08 22:51:42 +1300

    Wrote JavaDoc for all the screen enum values

--- src/main/java/seng202/team3/gui/Screen.java
+++ src/main/java/seng202/team3/gui/Screen.java
@@ -6,19 +6,77 @@ enum detailing necessary fields for LoadScreen method in FXWrapper
  @author Rafe Dunlop (rdu46), Krishna Sridhar (nsr36)
  */
 public enum Screen {
+    /**
+     * The home screen
+     */
     HOME("home_screen.fxml", false),
+
+    /**
+     * The navigation bar. Embeds the search screen, sign-in screen, profile screen, and help screen into it.
+     */
     NAVBAR("nav_bar.fxml", false),
+
+    /**
+     * The search screen.
+     */
     SEARCH("search_screen.fxml", true),
+
+    /**
+     * The sign-in screen. Is embedded into the navigation bar.
+     */
     SIGNINSCREEN("sign_in_screen.fxml", true),
+
+    /**
+     * The help screen. Is embedded into the navigation bar.
+     */
     HELPSCREEN("help_screen.fxml", true),
+
+    /**
+     * The profile tab pane. Is embedded into the profile screen. Embeds the wine list screen, profile list view screen,
+     * and tracking consumption screen into its tabs.
+     */
     PROFILETABPANE("profile_tab_pane.fxml", true),
+
+    /**
+     * The profile screen. Embeds the profile tab pane into it.
+     */
     PROFILESCREEN("profile_screen.fxml", false),
+
+    /**
+     * The wine lists screen. Embedded into the profile tab pane, in the same tab as profile list view screen. Shows all
+     * the user's wine lists. Opens the 'create a list' and 'delete a list' popups.
+     */
     WINELISTSSCREEN("profile_wine_lists_screen.fxml", false),
+
+    /**
+     * The tracking consumption screen.
+     */
     TRACKINGCONSUMPTIONSCREEN("tracking_consumption_screen.fxml", false),
+
+    /**
+     * The profile list view screen. Embedded into the profile tab pane, in the same tab as the wine lists view screen.
+     * Shows the wines in a certain list. Opens the cancel changes popup.
+     */
     PROFILELISTVIEWSCREEN("profile_list_view_screen.fxml", false),
+
+    /**
+     * The 'create a list' popup. Opened on top of and by the wine lists screen.
+     */
     CREATELISTPOPUP("create_list_pop_up.fxml", false),
+
+    /**
+     * The 'delete a list' popup. Opened on top of and by the wine lists screen.
+     */
     DELETELISTSPOPUP("deleting_lists_pop_up.fxml", false),
+
+    /**
+     * The cancel changes popup. Opened on top of and by the profile list view screen.
+     */
     CANCELCHANGESPOPUP("cancel_changes_pop_up.fxml", false),
+
+    /**
+     * The wine list select popup. Opened on top of and by the individual wine view in the search screen.
+     */
     WINELISTSELECTPOPUP("wine_list_select.fxml", false);
 
     /**
@@ -34,6 +92,7 @@ public enum Screen {
 
     /**
      * Constructor for the screen enum
+     *
      * @param fxmlFile filename of the fxml file of the current screen
      * @param hasNavBar boolean variable stating whether the screen will have the navigation bar
      */
