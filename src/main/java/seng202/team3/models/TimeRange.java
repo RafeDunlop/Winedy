f43c8abe6ecfbb878ee15e1a193be090744df1d8
hbo51 <hbo51@uclive.ac.nz>
2024-10-09 17:57:55 +1300

    Added JavaDoc comments to everything in the TimeRange.java Enum.

--- src/main/java/seng202/team3/models/TimeRange.java
+++ src/main/java/seng202/team3/models/TimeRange.java
@@ -11,20 +11,61 @@ import java.util.TimeZone;
 
 public enum TimeRange {
 
+    /**
+     * Represents the current week, from Monday till the current day
+     */
     THISWEEK("This week"),
+
+    /**
+     * Represents the previous week, from Monday to Sunday
+     */
     LASTWEEK("Last week"),
+
+    /**
+     * Represents the past two weeks, from Monday to the second Sunday after
+     */
     PASTTWOWEEKS("Past two weeks"),
+
+    /**
+     * Represents the current month, from the first of the month to the current day
+     */
     THISMONTH("This month"),
+
+    /**
+     * Represents the entire previous month
+     */
     LASTMONTH("Last month"),
+
+    /**
+     * Represents the current year, from the first of January to the current day
+     */
     THISYEAR("This year"),
+
+    /**
+     * Represents the entire last year
+     */
     LASTYEAR("Last year");
 
+    /**
+     * The String representation of the TimeRange value
+     */
     public final String strRep;
 
+    /**
+     * Constructor for the TimeEnum values, sets the string representation of the value to be the given String
+     *
+     * @param strRep the textual representation of the time value, as a string
+     */
     TimeRange(String strRep) {
         this.strRep = strRep;
     }
 
+    /**
+     * Returns the date range of the given TimeRange value
+     *
+     * @param range the TimeRange value of the requested date range
+     * @return a pair object containing two Date objects, the beginning date, and end date of the range
+     */
     public static Pair<Date, Date> getDateRange(TimeRange range) {
         Date now = LogManager.getInstance().getCurrentDate();
         Date soWeek = getSOWeek();
@@ -45,6 +86,15 @@ public enum TimeRange {
         };
     }
 
+    /**
+     * Returns a StringConverter for converting the given TimeRange value to and from its string representation.
+     * Defines/Overides two methods, toString which converts the TimeRange value to its string representation, and
+     * fromString which converts a String into its respective TimeRange value, if there is no corresponding TimeRange
+     * value, this is defaulted to THISWEEK.
+     *
+     *
+     * @return a StringConverter for converting TimeRange objects to and from their string representations
+     */
     public static StringConverter<TimeRange> getStringConverter() {
         return new StringConverter<>() {
             @Override
@@ -64,10 +114,20 @@ public enum TimeRange {
         };
     }
 
+    /**
+     * Returns a list containing all TimeRange values
+     *
+     * @return a list of all TimeRange values
+     */
     public static List<TimeRange> getAll() {
         return List.of(THISWEEK, LASTWEEK, PASTTWOWEEKS, THISMONTH, LASTMONTH, THISYEAR, LASTYEAR);
     }
 
+    /**
+     * Resets the Calendar instance to the beginning of the current day in the New Zealand Standard Time Zone (midnight)
+     *
+     * @return the calendar instance that has been reset
+     */
     private static Calendar getResetCalendar() {
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("NZDT"));
         cal.set(Calendar.HOUR_OF_DAY, 0);
@@ -77,12 +137,26 @@ public enum TimeRange {
         return cal;
     }
 
+    /**
+     * Returns a Date object representing the start of the current week in New Zealand Standard Time Zone.
+     * This will be Monday at midnight
+     *
+     * @return the Date object representing the start of the week
+     */
     private static Date getSOWeek() {
         Calendar cal = getResetCalendar();
         cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
         return new Date(cal.getTimeInMillis());
     }
 
+    /**
+     * Returns a Date object representing the start of the month in New Zealand Standard Time Zone.
+     * This will be the first of the month at midnight displacement many months in the future
+     *
+     * @param displacement an integer representing how many months in the future (or past if negative) this Date should
+     *                     be.
+     * @return the Date object representing the start of the requested month
+     */
     private static Date getSOMonth(int displacement) {
         Calendar cal = getResetCalendar();
         cal.set(Calendar.DAY_OF_MONTH, 1);
@@ -90,12 +164,18 @@ public enum TimeRange {
         return new Date(cal.getTimeInMillis());
     }
 
+    /**
+     * Returns a Date object representing the start of the year in New Zealand Standard Time Zone.
+     * This will be the first of the January at midnight displacement many years in the future
+     *
+     * @param displacement an integer representing how many years in the future (or past if negative) this Date should
+     *                     be.
+     * @return the Date object representing the start of the requested year
+     */
     private static Date getSOYear(int displacement) {
         Calendar cal = getResetCalendar();
         cal.set(Calendar.DAY_OF_YEAR, 1);
         cal.add(Calendar.DAY_OF_YEAR, displacement);
         return new Date(cal.getTimeInMillis());
     }
-
-
 }
