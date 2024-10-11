3fc82b582bfa3643b87181dfa6e7592a69f0e2e7
Hannah Botting (hbo51) <hbo51@uclive.ac.nz>
2024-10-12 01:56:47 +1300

    Adding missing documentation in DrinkerPreferenceModel.java
    Adding missing lines etc to improve consistent code style

--- src/main/java/seng202/team3/models/DrinkerPreferenceModel.java
+++ src/main/java/seng202/team3/models/DrinkerPreferenceModel.java
@@ -1,4 +1,5 @@
 package seng202.team3.models;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,13 +10,22 @@ import java.util.HashMap;
  * @author Steven Leishman (sle159)
  */
 public class DrinkerPreferenceModel {
+
+    /**
+     * The username of the Wine Drinker whose preference model this is
+     */
     private String username;
+
+    /**
+     * A hashmap that maps the preference types to their preference values
+     */
     private HashMap<String, Float> preferencesHashMap = new HashMap<>();
 
-    public DrinkerPreferenceModel(){}
+    public DrinkerPreferenceModel() {}
 
     /**
      * sets the arraylist of the
+     *
      * @param usernameToSet username for current drinker preference model
      */
     public void setUsername(String usernameToSet){
@@ -25,10 +35,11 @@ public class DrinkerPreferenceModel {
     /**
      * Takes two Arraylists of strings and floats of equal length representing the users preference model
      * and stores it in the preferences hash map
+     *
      * @param prefNames Arraylist of Strings representing preference types
      * @param prefNums Arraylist of Floats representing preference value for each string
      */
-    public void setHashMapValues(ArrayList<String> prefNames, ArrayList<Float> prefNums){
+    public void setHashMapValues(ArrayList<String> prefNames, ArrayList<Float> prefNums) {
         for (int i = 0; i < prefNames.size(); i++) {
             if (preferencesHashMap.get(prefNames.get(i)) == null) {
                 preferencesHashMap.put(prefNames.get(i).toLowerCase(), prefNums.get(i));
@@ -38,6 +49,7 @@ public class DrinkerPreferenceModel {
 
     /**
      * Returns the float preference value of a given string attribute
+     *
      * @param attributeToGet String representation of attribute to retrieve
      * @return the float score value of the preference requested
      */
@@ -47,6 +59,7 @@ public class DrinkerPreferenceModel {
 
     /**
      * Returns the Attribute score pair for all attributes in the form of a hashmap
+     *
      * @return preferenceHashMap - the hash map of String attributes mapped to float scores
      */
     public HashMap<String, Float> getPreferencesHashMap(){
@@ -55,6 +68,7 @@ public class DrinkerPreferenceModel {
 
     /**
      * Get the username stored with this preference mode
+     *
      * @return String of username
      */
     public String getUsername() {
@@ -64,13 +78,20 @@ public class DrinkerPreferenceModel {
     /**
      * Returns the current abv preference score
      * ABV works differently and is always based of the users current preferences
+     *
      * @return current abv preference score
      */
     public float getABV(){
         return preferencesHashMap.get("abv");
     }
 
-    public int getNumColumns(){
+    /**
+     * Returns the number of columns/attributes stored in the hash map by iterating over the key set and incrementing a
+     * counter
+     *
+     * @return the number of attributes in the hash map
+     */
+    public int getNumColumns() {
         int i = 0;
         for(String key : preferencesHashMap.keySet()){
             i++;
