393f6b5cc11f37c7c69c0d6df5900f428e909c16
rdu46 <rdu46@uclive.ac.nz>
2024-10-13 00:29:27 +1300

    fixed problem where short dates/months .e.g 9, 1 vs 10 were padded incorrectly (1 -> 10) vs (1 -> 01).
    added error handling for too-high amount values (10 max)
    
    added a get prompt text which sets the prompt text based on whether the user is logging by bottles or glasses

--- src/main/java/seng202/team3/services/LogManager.java
+++ src/main/java/seng202/team3/services/LogManager.java
@@ -211,9 +211,9 @@ public class LogManager {
 
     public String getLogDateString(WineLog wineLog) {
         LocalDate date = wineLog.getDate().toLocalDate();
-        return ((date.getDayOfMonth() < 10) ? date.getDayOfMonth() + "0" : date.getDayOfMonth()) +
+        return ((date.getDayOfMonth() < 10) ? "0" + date.getDayOfMonth(): date.getDayOfMonth()) +
                 "/" +
-                ((date.getMonthValue() < 10) ? date.getMonthValue() + "0" : date.getMonthValue()) +
+                ((date.getMonthValue() < 10) ? "0" + date.getMonthValue(): date.getMonthValue()) +
                 "/" +
                 date.getYear();
     }
@@ -255,6 +255,8 @@ public class LogManager {
                 return new Pair<>(false, "Amount must be positive");
             } else if (value == 0) {
                 return new Pair<>(false, "Amount cannot be 0");
+            } else if (value > 10){
+                return new Pair<>(false, "Amount cannot be more than 10");
             } else {
                 return new Pair<>(true,"errorDisplayLabel");
             }
@@ -289,6 +291,12 @@ public class LogManager {
                 return Integer.parseInt(s.replace("AM", ""));
             }
         };
+    }
+
+    public String getAmtPromptText(boolean isBottles) {
+        return "Enter how many " +
+                ((isBottles) ? "bottles" : "glasses") +
+                "you had.";
     }
 
     public void setTimeRange(TimeRange toSet) {
