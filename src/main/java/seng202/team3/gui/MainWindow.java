73f96ba6d756549335eaa32b25e2c6ed201f91c2
nsr36 <nsr36@uclive.ac.nz>
2024-10-13 21:26:39 +1300

    Removed unnecessary css references

--- src/main/java/seng202/team3/gui/MainWindow.java
+++ src/main/java/seng202/team3/gui/MainWindow.java
@@ -42,7 +42,6 @@ public class MainWindow extends Application {
                 "/css/date_combo_box.css",
                 "/css/help_screen_contents_button.css",
                 "/css/home_screen_button.css",
-                "/css/home_screen_text.css",  //ToDo remove if unused
                 "/css/individual_wine_view_scroll_pane.css",
                 "/css/nav_bar_button.css",
                 "/css/nav_bar_rectangle.css",
@@ -62,7 +61,6 @@ public class MainWindow extends Application {
                 "/css/white_red_wine_rectangle.css",
                 "/css/fifteen_combo_box.css",
                 "/css/slider.css",
-                "/css/sign_in_screen_background_rectangle.css",  //TODO remove if unused
                 "/css/sign_in_screen_text_field.css",
                 "/css/overlay.css",
                 "/css/wine_pagination.css",
